a46c97f8dc3b659bc511ae666ff3c286a4cf8d99
Juan Melo <jcmelos@unal.edu.co>
2026-06-28 14:10:33 -0500

    creaciond el crud

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -5,6 +5,18 @@ const Task=require("../models/Task")
 
 const creartarea= async(req,res)=>{
     try{
+         const tareaExiste = await Task.findOne({
+            titulo: titulo,
+            usuario: req.user.id
+        });
+
+        if (tareaExiste) {
+            return res.status(400).json({
+                msg: "Ya existe una tarea con ese título."
+            });
+        }
+
+
         const task=new Task({
             titulo:req.body.titulo,
             usuario:req.user.id});    
@@ -18,6 +30,50 @@ const creartarea= async(req,res)=>{
 
 }
 
+const traertarea= async(req,res)=>{
+    try{
+        const tasks = await Task.findOne({
+            usuario:req.user.id
+        })
+        
+        res.json(tasks);
+    }catch(error){
+        res.status(500).json({error:`La busqueda de la tarea fallo  ${error.message}`})
+    }
+
+}
+
+
+const actualizartarea= async(req,res)=>{
+    try{
+        const tasks = await Task.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        )
+        
+        res.json(tasks);
+    }catch(error){
+        res.status(500).json({error:`Erro al actualizar tarea  ${error.message}`})
+    }
+
+}
+
+const eliminartarea= async(req,res)=>{
+    try{
+        const tasks = await Task.findByIdAndDelete(
+        )
+        
+        res.json("El elemento hay sido eliminado");
+    }catch(error){
+        res.status(500).json({error:`Erro al eliminar la tarea  ${error.message}`})
+    }
+
+}
+
 module.exports={
-    creartarea
+    creartarea,
+    traertarea,
+    actualizartarea,
+    eliminartarea
 }
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,9 +1,15 @@
 const express= require("express");
 const router=express.Router();
-const { creartarea } = require("../controllers/task.controller");
+const { creartarea, traertarea, actualizartarea, eliminartarea  } = require("../controllers/task.controller");
 const validartoken=require("../middlewares/auth.middleware")
 
 router.post("/creartarea",validartoken,creartarea);
+
+router.get("/traertarea",validartoken,traertarea);
+
+router.put("/:id",validartoken,actualizartarea);
+
+router.delete("/:id",validartoken,eliminartarea);
 
 
 
